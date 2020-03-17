6e1a225368d794b710eacb507b168941c0eb154e
Daniel Sasser <danielbsasser@gmail.com>
2020-03-17 13:11:15 +0000

    Automatically backed up by Learn

---
+++ lib/dog.rb
@@ -0,0 +1,57 @@
+class Dog
+
+  attr_accessor :name, :breed, :id
+
+  def initialize(id: nil, name:, breed:)
+    @id = id
+    @name = name
+    @breed = breed
+  end
+
+  def self.create_table
+    sql = <<-SQL
+    CREATE TABLE dogs (
+    id INTEGER PRIMARY KEY,
+    name TEXT,
+    breed TEXT
+    );
+    SQL
+
+    DB[:conn].execute(sql)
+  end
+
+  def self.drop_table
+    sql = "DROP TABLE dogs"
+
+    DB[:conn].execute(sql)
+  end
+
+  def save
+    sql = "INSERT INTO dogs (name, breed) VALUES (?, ?)"
+    DB[:conn].execute(sql, self.name, self.breed)
+    @id = DB[:conn].execute("SELECT last_insert_rowid() FROM dogs")[0][0]
+    self
+  end
+
+  def self.create(info)
+    dog = self.new(info)
+    dog.save
+    dog
+  end
+
+  def self.new_from_db(row)
+    id = row[0]
+    name = row[1]
+    breed = row[2]
+    dog = self.new(id: id, name: name, breed: breed)
+    dog
+  end
+
+  def self.find_by_id(id)
+    dog = DB[:conn].execute("SELECT * FROM dogs WHERE id = ?", id)[0]
+    self.new_from_db(dog)
+  end
+
+  def self.find_or_create_by
+
+end
